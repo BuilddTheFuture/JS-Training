56dc06de9419dad14633465cfdc9512d709a9d76
BuilddTheFuture <107243525+BuilddTheFuture@users.noreply.github.com>
2022-06-24 14:27:46 +0300

    new commit

---
+++ js/script.js
@@ -0,0 +1,102 @@
+'use strict';
+
+// let mood = "Nice day"; // Объявление переменной
+// console.log (mood); // Вывели в консоль
+// alert (mood); // Вывели на экран переменную 
+
+// let name_stuff, age, sex;
+//     name_stuff = "Igor";
+//     age = "28";
+//     sex = "man";
+// alert (name_stuff, age, sex);
+
+// let age;
+// age=1;
+// alert(age);
+
+// age=30;
+// alert(age);
+
+
+// let number = (1+2+3)/(1*2);
+// alert (number);
+
+// let a = 10, b = 5, result;
+// result = a - b;
+// alert(result);
+
+// let a = 1.5, b = 0.75, result;
+// result = a + b;
+// alert(result);
+
+// let a = 13, b = 5, diff;
+// diff = a % b; // найдет остаток от деления - 3
+// alert(diff);
+
+// let a = 3 * 2 ** 3 * 3;
+// alert(a);
+
+// let my_name = 'Ihor', surname = 'Kahozhkin';
+// console.log (my_name + surname) // вывод в консоле
+// alert (my_name + surname) // вывод в на экран
+
+// let my_name = 'Ihor', surname = 'Kahozhkin';
+// alert (my_name + ' ' + surname) // вывод в на экран
+
+// let str1 = 'xxx';
+// let str2 = 'yyy';
+// alert(`aaa${str1}bbb${str2}ccc`)
+
+// let userAge = 28;
+// let userInfo = `фрилансеру ${userAge}`;
+// console.log(userInfo);
+
+// let $ = undefined; // переменная не определена, т.е undefined 
+// alert ($);
+
+// alert(-10 / 0); // выведет -Infinity
+// console.log(10 / 0); // выведет Infinity
+
+// let userHight = -145 / 0;
+// console.log(userHight);
+// console.log(typeof userHight);
+
+// let num1 = 123, num2 = 456;
+// console.log(num1 + num2);
+// console.log('123');
+
+// const PI = 3.1444;
+// let c, d=10;
+// c = PI * d;
+// console.log (c);
+
+// let a = '5' + '2';
+// alert(a);
+
+// let a = '5' % '2';
+// alert(a);
+
+// let a = '5s' + '2';
+// alert(a);
+
+// let a = '5' * 1 + '2' * 1;
+// alert(a);
+
+// let a = '' + 3 + 1;
+// alert(a);
+
+// let a = '10', b = '20';
+// console.log(Number(a) + Number(b));
+
+// alert( '2' + Number('3') );
+
+// let aaa = parseInt('5px');
+// let bbb = parseInt('6px');
+// console.log(aaa+bbb);
+
+// let aaa = parseFloat('5.5px');
+// let bbb = parseFloat('6.25px');
+// console.log(aaa+bbb+'px');
+
+// let num = 72354, str = String(num);
+// console.log(str.length);
